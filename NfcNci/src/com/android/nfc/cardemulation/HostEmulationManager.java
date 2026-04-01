5d01226993faa40c16f53cf25fe621f260a2fb0f
Kyle Hsiao <kylehsiao@google.com>
2026-04-01 03:23:24 -0700

    [ASB 2026-06] Fix permanent BAL grant via onServiceConnected race
    Bug: 496735702
    Test: atest CtsNfcTestCases
    Test: atest NfcNciUnitTests
    Test: atest CtsNfcHceMultiDeviceTestCase
    Flag: EXEMPT BUGFIX
    Change-Id: Idc9a780162c4d4dacbbba5c78e438c49da0a50e5
    Merged-In: Idc9a780162c4d4dacbbba5c78e438c49da0a50e5
    (cherry picked from commit 6564c573280aad995b6e71536a1b8c190c63afea)

--- NfcNci/src/com/android/nfc/cardemulation/HostEmulationManager.java
+++ NfcNci/src/com/android/nfc/cardemulation/HostEmulationManager.java
@@ -1752,6 +1752,13 @@ public class HostEmulationManager {
                                 preferredUserAndService.getComponentName();
                 /* Service is already deactivated and not preferred, don't bind */
                 if (mState.get() == STATE_IDLE && !name.equals(preferredServiceName)) {
+                    try {
+                        mContext.unbindService(this);
+                    } catch (IllegalArgumentException e) {
+                        Log.w(TAG, "Failed to unbind " + name, e);
+                    }
+                    mComponentNameToConnectionsMap.remove(
+                            new ComponentNameAndUser(mUserId, name));
                     return;
                 }
                 Messenger messenger = new Messenger(service);
